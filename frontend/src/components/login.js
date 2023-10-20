d30f7b0347007d34a6cd5351fdc384abeebbd8f0
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-10-19 20:56:42 -0400

    MUI installed and add some  MUI styles and btns

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,10 +1,18 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import Button from '@mui/material/Button';
+
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <div>
+      <Button variant="contained" color="primary" onClick={() => loginWithRedirect()}>
+        Login
+      </Button>
+    </div>
+  );
 };
 
 export default LoginButton;
