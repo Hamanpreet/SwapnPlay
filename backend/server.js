20ed13e804b2111a547fdd6d4e0807b57e7420e2
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-10-22 17:14:21 -0400

    add router

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,35 @@
 require("dotenv").config();
-
 const express = require("express");
-// To log details about incoming HTTP requests
+const app = express();
+const PORT = process.env.PORT || 8080;
+const helmet = require('helmet');
 const morgan = require("morgan");
 const cors = require('cors');
+const debug = require('debug')('app:startup');
+const toys = require('./routes/toys')
+const reviews = require('./routes/reviews')
+const matches = require('./routes/matches')
+
+//Middlewares
+// To log details about incoming HTTP requests
+app.use(express.json()); //req.body
+app.use(helmet());
+app.use(cors());
+app.use('/api/toys',toys);
+app.use('/api/reviews',reviews);
+app.use('/api/matches',matches);
+
+if (app.get('env') === 'development'){
+  app.use(morgan('tiny'));
+  debug('Application started in development mode!'); //export DEBUG=app:startup in environment variable
+}
 
 
-
-const PORT = process.env.PORT || 8080;
-const app = express();
-
+app.get('/', (req, res)=>{
+  res.send('Hello');
+});
 
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`listening on port ${PORT}`);
 });
