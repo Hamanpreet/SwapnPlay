7b4db32f66e0c10bcb86182d7495c5b38f37f074
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-10-22 20:56:31 -0400

    setup pg pool for db connectivity

--- Backend/routes/toys.js
+++ Backend/routes/toys.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const database = require("../db/queries/toys");
+
 router.get('/',(req, res)=>{
-  res.send('send all toys!');
+  database.getToys()
+  .then((toys) => {
+    res.send(toys);
+  })
+  .catch((err)=>{
+    console.log(`An error occurred: ${err}`)
+  });
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
+// load .env data into process.env
 require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const helmet = require('helmet');
 const morgan = require("morgan");
 const cors = require('cors');
+//const config = require('config');
 const debug = require('debug')('app:startup');
+
+//configuration
+// console.log(`PGUSER: ${config.get('PGUSER')}`)
+// console.log(`Environment: ${process.env.NODE_ENV}`)
+
+// Separated Routes for each Resource
 const toys = require('./routes/toys')
 const reviews = require('./routes/reviews')
 const matches = require('./routes/matches')
@@ -15,6 +24,8 @@ const matches = require('./routes/matches')
 app.use(express.json()); //req.body
 app.use(helmet());
 app.use(cors());
+
+// Mount all resource routes
 app.use('/api/toys',toys);
 app.use('/api/reviews',reviews);
 app.use('/api/matches',matches);
