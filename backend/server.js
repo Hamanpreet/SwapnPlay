f2c2efc2e6f3b7191e3048746a030131a80b5d13
Hamanpreet <haman42626@gmail.com>
2023-10-23 13:18:24 -0400

    Fix the spelling error in backend

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,20 @@
+require("dotenv").config();
 const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 8080;
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./doc/swagger'); // Import your generated Swagger spec
 const helmet = require('helmet');
 const morgan = require("morgan");
 const cors = require('cors');
-//const config = require('config');
 const debug = require('debug')('app:startup');
-const app = express();
-
-//configuration
-// console.log(`PGUSER: ${config.get('PGUSER')}`)
-// console.log(`Environment: ${process.env.NODE_ENV}`)
-
-// Separated Routes for each Resource
-const toys = require('./routes/toys')
-const reviews = require('./routes/reviews')
-const matches = require('./routes/matches')
+const toys = require('./src/routes/toys')
+const reviews = require('./src/routes/reviews')
+const matches = require('./src/routes/matches')
 
 //Middlewares
 // To log details about incoming HTTP requests
 app.use(express.json()); //req.body
 app.use(helmet());
 app.use(cors());
-
-// Serve Swagger UI at a specific route
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// Mount all resource routes
 app.use('/api/toys',toys);
 app.use('/api/reviews',reviews);
 app.use('/api/matches',matches);
