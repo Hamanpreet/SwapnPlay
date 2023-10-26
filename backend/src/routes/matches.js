98687fdc58c0a31f0458a4cba53497b4719c64d9
madiha-waqar <mail2madihawaqar@gmail.com>
2023-10-26 14:59:09 -0400

    Create post request for new match

--- backend/src/routes/matches.js
+++ backend/src/routes/matches.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res)=>{
-  res.send('send all matches!');
+const { requestMatch } = require("../../db/queries/matches");
+
+// Set up a route to create a new match request
+router.post('/', (req, res) => {
+  const body = req.body;
+  requestMatch(body)
+    .then((matches) => {
+      console.log("Match request created successfully")
+      res.send(matches);
+    })
+    .catch((err) => {
+      console.log(`An error occurred: ${err}`)
+    });
 });
 
 module.exports = router;
