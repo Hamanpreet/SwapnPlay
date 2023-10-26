3175b96a1453b99d0015754be133498bd3119cac
madiha-waqar <mail2madihawaqar@gmail.com>
2023-10-26 14:56:46 -0400

    Create match request db query

---
+++ backend/db/queries/matches.js
@@ -0,0 +1,26 @@
+const db = require("../connection");
+
+
+// Return all matches for logged in user
+const requestMatch = (data) => {
+  console.log("Entering DB");
+
+  const query = `
+    INSERT INTO match (status, toy_id, toy_in_exchange_id, user1_id, user2_id, created_at)
+    VALUES ($1, $2, $3, $4, $5, $6) 
+    RETURNING *;
+    `;
+  
+  return db
+    .query(query, [data.status, data.toy_id, data.toy_in_exchange_id, data.user1_id, data.user2_id, new Date()])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((error) => {
+      console.error('Error in creating new match request :', error);
+      throw error;
+    });
+};
+module.exports = {
+   requestMatch
+};
