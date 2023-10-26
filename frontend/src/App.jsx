ae8e1b67d75429bb78bfad540cb06c28ef782982
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-10-26 19:59:01 -0400

    add userprofile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Toy from './components/Toy';
 import NotFound from './components/NotFound';
 import NewToy from './components/NewToy';
 import ToyList from "./components/ToyList";
+import UserProfile from './components/UserProfile';
 import Home from './components/Home';
 import { useState } from "react";
 import TopNavigationBar from './components/TopNavigationBar'
@@ -35,6 +36,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <Router>
           <Routes>
+            <Route path='/userprofile' element={<UserProfile/>} />
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/toys/:id" element={<Toy />} />
