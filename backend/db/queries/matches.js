046a8a1fa4173b3a57155fdf55dd2daf77e8cc50
madiha-waqar <mail2madihawaqar@gmail.com>
2023-10-27 19:43:54 -0400

    Build query to create a new match in DB

--- backend/db/queries/matches.js
+++ backend/db/queries/matches.js
@@ -1,26 +1,26 @@
 const db = require("../connection");
 
 
-// Return all matches for logged in user
-const requestMatch = (data) => {
-  console.log("Entering DB");
+// Insert new match into the database
+const createtNewMatch = (data) => {
+  console.log("Entering DB to create new match");
 
   const query = `
     INSERT INTO match (status, toy_id, toy_in_exchange_id, user1_id, user2_id, created_at)
     VALUES ($1, $2, $3, $4, $5, $6) 
     RETURNING *;
     `;
-  
   return db
     .query(query, [data.status, data.toy_id, data.toy_in_exchange_id, data.user1_id, data.user2_id, new Date()])
     .then((result) => {
       return result.rows[0];
     })
     .catch((error) => {
-      console.error('Error in creating new match request :', error);
+      console.error('Error in creating new match:', error);
       throw error;
     });
 };
+
 module.exports = {
-   requestMatch
+  createtNewMatch
 };
