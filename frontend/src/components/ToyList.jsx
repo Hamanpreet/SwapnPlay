d685394474632a480c7120cbf103b76417f2a0f6
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-11-03 16:00:32 -0400

    display toys on toys page and style page

--- frontend/src/components/ToyList.jsx
+++ frontend/src/components/ToyList.jsx
@@ -108,8 +108,17 @@ const ToyListPage = (props) => {
                 </Grid>
               ))
             : toyList.map((toy) => (
-                <Grid item key={toy.id} xs={12} sm={6} md={4} lg={4}>
-                  <Card>
+              <Grid item key={toy.id} xs={12} sm={6} md={4} lg={4}>
+              <Card style={{ backgroundColor: '#f0f0f0' }}>
+                <Grid container>
+                  <Grid item xs={4}> {/* Adjust the width of the image column as needed */}
+                    <img
+                      src={toy.url} // Replace with the actual URL of the toy image
+                      alt="Toy"
+                      style={{ width: '100%', height: 'auto' }}
+                    />
+                  </Grid>
+                  <Grid item xs={8}> {/* Adjust the width of the details column as needed */}
                     <CardContent>
                       <Typography variant="h6" component="div">
                         {toy.title}
@@ -126,13 +135,17 @@ const ToyListPage = (props) => {
                     </CardContent>
                     <Button
                       variant="contained"
-                      color="primary"
+                      color="success"
                       onClick={() => handleOpenModal(toy)}
+                      style={{ margin: 'auto', marginBottom: '16px' }}
                     >
                       Request to Match
                     </Button>
-                  </Card>
+                  </Grid>
                 </Grid>
+              </Card>
+            </Grid>
+            
               ))}
         </Grid>
       </div>
