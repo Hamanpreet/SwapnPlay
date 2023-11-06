88826a48f63d78d2b1d48038934a71428199a49b
madiha-waqar <mail2madihawaqar@gmail.com>
2023-11-05 22:10:41 -0500

    Changes for deployment

---
+++ backend/config/development.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  PGHOST:process.env.PGHOST,
+  PGDATABASE:process.env.PGDATABASE,
+  PGUSER:process.env.PGUSER,
+  PGPASSWORD:process.env.PGPASSWORD,
+  PGPORT:process.env.PGPORT,
+  PORT:process.env.PORT,
+  OPEN_AI_KEY:process.env.OPEN_AI_KEY
+}
--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,13 +1,13 @@
 // PG database client/connection setup
-const config = require('config');
+const config = require('../config/development');
 const { Pool } = require('pg');
 
 const dbParams = {
-  host: config.get('PGHOST'), //process.env.PGHOST,
-  database: config.get('PGDATABASE'), // application breaks when variable name is name
-  user: config.get('PGUSER'),
-  password: config.get('PGPASSWORD'),
-  port: config.get('PGPORT'),
+  host: config.PGHOST, //process.env.PGHOST,
+  database: config.PGDATABASE, // application breaks when variable name is name
+  user: config.PGUSER,
+  password: config.PGPASSWORD,
+  port: config.PGPORT,
   //ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 };
 const db = new Pool(dbParams);
@@ -15,4 +15,3 @@ const db = new Pool(dbParams);
 db.connect().catch(e => console.log(`Error connecting to Postgres server:\n${e}`));;
 
 module.exports = db;
-``
